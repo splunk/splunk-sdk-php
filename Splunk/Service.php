cbc9c6cbe8bf66a9567d8799fe2cf55ff6447e95
David Foster <davidfstr@gmail.com>
2012-12-13 12:25:22 -0800

    Add Service.{search,oneshotSearch} convenience methods to align with other SDKs. (DVPL-1457)

--- Splunk/Service.php
+++ Splunk/Service.php
@@ -63,4 +63,51 @@ class Splunk_Service extends Splunk_Context
     {
         return new Splunk_Collection($this, 'saved/searches/', 'Splunk_SavedSearch');
     }
+    
+    // === Convenience ===
+    
+    /**
+     * Creates a new search job.
+     * 
+     * @param string $search    The search query for the job to perform.
+     * @param array $args   (optional) Job-specific creation arguments,
+     *                      merged with {
+     *     'namespace' => (optional) {Splunk_Namespace} The namespace in which
+     *                    to create the entity. Defaults to the service's
+     *                    namespace.
+     * }
+     *                      For details, see the
+     *                      "POST search/jobs"
+     *                      endpoint in the REST API Documentation.
+     * @return Splunk_Job
+     * @throws Splunk_IOException
+     * @link http://docs.splunk.com/Documentation/Splunk/4.3.3/RESTAPI/RESTsearch#search.2Fjobs
+     */
+    public function search($search, $args=array())
+    {
+        return $this->getJobs()->create($search, $args);
+    }
+    
+    /**
+     * Executes the specified search query and returns results immediately.
+     * 
+     * @param string $search    The search query for the job to perform.
+     * @param array $args   (optional) Job-specific creation arguments,
+     *                      merged with {
+     *     'namespace' => (optional) {Splunk_Namespace} The namespace in which
+     *                    to create the entity. Defaults to the service's
+     *                    namespace.
+     * }
+     *                      For details, see the
+     *                      "POST search/jobs"
+     *                      endpoint in the REST API Documentation.
+     * @return string           The search results, which can be parsed with
+     *                          Splunk_ResultsReader.
+     * @throws Splunk_IOException
+     * @link http://docs.splunk.com/Documentation/Splunk/4.3.3/RESTAPI/RESTsearch#search.2Fjobs
+     */
+    public function oneshotSearch($search, $args=array())
+    {
+        return $this->getJobs()->createOneshot($search, $args);
+    }
 }
