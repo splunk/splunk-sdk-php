882baea16ae72723c8cf1fd30c6933147f1c434f
adamziaja <aziaja@gmail.com>
2017-02-19 00:06:04 +0100

    fix

--- tests/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'settings.default.php';
-// (warn if file not found)
-include_once 'settings.local.php';
-// (ignore if file not found)
-if (getenv('HOME'))
-    @include_once getenv('HOME') . '/.splunk-phpsdk-test-settings.php';
-print "Testing on PHP ".phpversion()."\n";
